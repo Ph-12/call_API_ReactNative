2ce68857fbfe69882f9c931203cb235534bfd314
Ph <sleephil@gmail.com>
2021-08-23 13:35:21 -0300

    adjustment

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,21 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 export default function HomeScreen({ navigation }) {
 	return (
           <View style={{ flex:1, flexDirection: 'row', justifyContent:'center', alignItems: 'center'}}>
-	  <View style={{ marginRight: 10 }}>
-	    <Button
-		 title="Go to Class"
-		 onPress={() => navigation.navigate('Class')}
-	    />
-	    
-	  </View>
+               <View style={{ marginRight: 10 }}>
+               <Button
+                    title="Go to Class"
+                    onPress={() => navigation.navigate('Class')}
+               />
+               
+               </View>
 
-          <View style={{ marginLeft: 10}}>
-          <Button
-          title="Go to Function"
-          onPress={() => navigation.navigate('Function')}
-          />
+                    <View style={{ marginLeft: 10}}>
+                    <Button
+                    title="Go to Function"
+                    onPress={() => navigation.navigate('Function')}
+                    />
 
+                    </View>
           </View>
-      </View>
 	);
    }
